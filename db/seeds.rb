2ce24512293b6cbdf38fbe42e0d370cc4df31c86
Nicolas Amiel <n.amiel.andersen@gmail.com>
2017-11-22 17:36:53 +0100

    2nd not finished

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Flat.destroy_all
+Flat.create!([
+  {
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+  },
+  {
+  name: 'A tremendous squat in Berlin',
+  address: '15 Haupsstrasse',
+  description: 'You come, you go, whatever you want in this lovely space hidden behind big buildings in the hippies district',
+  price_per_night: 25,
+  number_of_guests: 10
+  },
+  {
+  name: 'Huge apartment in Paris',
+  address: '15 rue de Montmartre',
+  description: 'A cliché of the parisian life, large livingroom and a wide terrace for parties!',
+  price_per_night: 120,
+  number_of_guests: 4
+  },
+])
+
